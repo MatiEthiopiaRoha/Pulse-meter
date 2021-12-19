5c67abd69e3da2ad5852992336ef97a2f0ea4612
Mati Ethiopia <91181733+MatiEthiopiaRoha@users.noreply.github.com>
2021-12-19 21:39:11 +0000

    Update Form1.cs

--- Pulse meter/Pulse meter/Form1.cs
+++ Pulse meter/Pulse meter/Form1.cs
@@ -107,14 +107,6 @@ namespace Pulse_meter
                 serialDataChart.TriggeredUpdate(data);
             }
         }
-        private void label3_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
